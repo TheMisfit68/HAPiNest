de396c62f946c6d658b50fac98b465ca0cfa699a
TheMisfit68 <janverrept@me.com>
2021-01-22 23:32:02 +0100

    Prioritised characteristic changes

--- PLC/PLC Functionality/DimmableLight.swift
+++ PLC/PLC Functionality/DimmableLight.swift
@@ -13,108 +13,115 @@ import ModbusDriver
 import JVCocoa
 
 public class DimmableLight:PLCclass, Parameterizable, AccessoryDelegate, AccessorySource{
-    
-    // MARK: - HomeKit Accessory binding
-    
-    typealias AccessorySubclass = Accessory.Lightbulb
-    
-    private var characteristicChanged:Bool = false
-    var hkAccessoryPowerState:Bool = false{
-        didSet{
-            // Only when circuit is idle
-            // send the feedback upstream to the Homekit accessory,
+	
+	// MARK: - HomeKit Accessory binding
+	
+	typealias AccessorySubclass = Accessory.Lightbulb
+	
+	private var characteristicChanged:Bool = false
+	
+	var hkAccessoryPowerState:Bool = false{
+		didSet{
+			// Only when circuit is idle
+			// send the feedback upstream to the Homekit accessory,
 			// provides a more stable experience
-            if  !characteristicChanged{
-                accessory.lightbulb.powerState.value = hkAccessoryPowerState
-            }
-        }
-    }
-    var hkAccessoryBrightness:Int = 100{
-        didSet{
-            // Only when circuit is idle
-            // send the feedback upstream to the Homekit accessory,
+			if  !characteristicChanged{
+				accessory.lightbulb.powerState.value = hkAccessoryPowerState
+			}
+		}
+	}
+	var hkAccessoryBrightness:Int = 100{
+		didSet{
+			// Only when circuit is idle
+			// send the feedback upstream to the Homekit accessory,
 			// provides a more stable experience
-            if  !characteristicChanged{
-                accessory.lightbulb.brightness?.value = hkAccessoryBrightness
-            }
-        }
-    }
-    
-    func handleCharacteristicChange<T>(accessory:Accessory,
-                                       service: Service,
-                                       characteristic: GenericCharacteristic<T>,
-                                       to value: T?){
-        characteristicChanged.set()
-        
-        // Handle Characteristic change depending on its type
-        switch characteristic.type{
-        case CharacteristicType.powerState:
-            
-            hkAccessoryPowerState = value as? Bool ?? hkAccessoryPowerState
-            
-        case CharacteristicType.brightness:
-            
-            hkAccessoryBrightness = value as? Int ?? hkAccessoryBrightness
-            
-        default:
-            Debugger.shared.log(debugLevel: .Warning, "Unhandled characteristic change for accessory \(name)")
-        }
-    }
-    
-    
-    // MARK: - PLC IO-Signal assignment
-    
-    var outputSignal:AnalogOutputSignal{
-        plc.signal(ioSymbol:instanceName) as! AnalogOutputSignal
-    }
-    
-    
-    // MARK: - PLC Parameter assignment
-    
-    public func assignInputParameters(){
-        
-        if characteristicChanged{
-            powerState = hkAccessoryPowerState
-            brightness = hkAccessoryBrightness
-            
-            characteristicChanged.reset()
-        }
-        
-    }
-    
-    public func assignOutputParameters(){
-        outputSignal.scaledValue = Float(brightness)
-        
-        hkAccessoryPowerState = powerState
-        hkAccessoryBrightness = brightness
-    }
-    
-    
-    // MARK: - PLC Processing
-    
-    private let switchOffLevelDimmer:Int = 15
-    private var previousbrightness:Int = 15
-        
-    var powerState:Bool = false{
-        didSet{
-            if powerState != oldValue{
-                brightness = powerState ? previousbrightness : 0
-            }
-        }
-    }
-    
-    var brightness:Int = 0{
-        didSet{
-            if brightness != oldValue{
-                // When swicthed on, remember each new brightnesslevel,
-                // to be used as a startlevel later on
-                if brightness > switchOffLevelDimmer{
-                    previousbrightness = brightness
-                }
-            }
-        }
-    }
-    
-    
+			if  !characteristicChanged{
+				accessory.lightbulb.brightness?.value = hkAccessoryBrightness
+			}
+		}
+	}
+	
+	func handleCharacteristicChange<T>(accessory:Accessory,
+									   service: Service,
+									   characteristic: GenericCharacteristic<T>,
+									   to value: T?){
+		characteristicChanged.set()
+		
+		// Handle Characteristic change depending on its type
+		switch characteristic.type{
+			case CharacteristicType.powerState:
+				
+				hkAccessoryPowerState = value as? Bool ?? hkAccessoryPowerState
+				
+			case CharacteristicType.brightness:
+				
+				hkAccessoryBrightness = value as? Int ?? hkAccessoryBrightness
+				
+			default:
+				Debugger.shared.log(debugLevel: .Warning, "Unhandled characteristic change for accessory \(name)")
+		}
+	}
+	
+	
+	// MARK: - PLC IO-Signal assignment
+	
+	var outputSignal:AnalogOutputSignal{
+		plc.signal(ioSymbol:instanceName) as! AnalogOutputSignal
+	}
+	
+	
+	// MARK: - PLC Parameter assignment
+	
+	public func assignInputParameters(){
+		
+		if brightness == nil {
+			brightness = Int(outputSignal.scaledValue)
+			powerState = (brightness >= switchOffLevelDimmer)
+		}else if characteristicChanged{
+			
+			if powerState != hkAccessoryPowerState{
+				powerState = hkAccessoryPowerState
+			}else{
+				brightness = hkAccessoryBrightness
+			}
+		}
+		
+	}
+	
+	public func assignOutputParameters(){
+		outputSignal.scaledValue = Float(brightness)
+		
+		hkAccessoryPowerState = powerState
+		hkAccessoryBrightness = brightness
+		characteristicChanged.reset()
+	}
+	
+	
+	// MARK: - PLC Processing
+	
+	private let switchOffLevelDimmer:Int = 15
+	private var previousbrightness:Int = 15
+	
+	var powerState:Bool = false{
+		didSet{
+			if powerState != oldValue{
+				brightness = powerState ? previousbrightness : 0
+			}
+		}
+	}
+	
+	var brightness:Int! = nil{
+		didSet{
+			if brightness != oldValue{
+				// When swicthed on, remember each new brightnesslevel,
+				// to be used as a startlevel later on
+				if brightness > switchOffLevelDimmer{
+					previousbrightness = brightness
+				}
+			}
+		}
+	}
+	
+	
 }
 
