debbc0d7df05bafd87e81252a7aa3a17921cf19e
TheMisfit68 <janverrept@me.com>
2021-02-01 16:23:40 +0100

    fixing milight hue change

--- AccessoryDelegates/MilightDelegate.swift
+++ AccessoryDelegates/MilightDelegate.swift
@@ -12,54 +12,59 @@ import JVCocoa
 import MilightDriver
 
 class MilightDelegate:AccessoryDelegate {
-    
-    let name:String
-    let driver:MilightDriver
-    let zone:MilightZone
-    
-    public init(name:String,
-                driver:MilightDriver,
-                zone:MilightZone
-    ) {
-        self.name = name
-        self.driver = driver
-        self.zone = zone
-    }
-    
-    func handleCharacteristicChange<T>(accessory:Accessory,
-                                       service: Service,
-                                       characteristic: GenericCharacteristic<T>,
-                                       to value: T?){
-        
-        // Handle Characteristic change depending on its type
-        switch characteristic.type{
-        case CharacteristicType.powerState:
-            
-            let poweredOn = characteristic.value as! Bool
-            let action = poweredOn ? MilightAction.on : MilightAction.off
-            driver.executeCommand(mode: .rgbwwcw, action: action, zone: zone)
-            
-        case CharacteristicType.brightness:
-            
-            let brightness = characteristic.value as! Int
-            driver.executeCommand(mode: .rgbwwcw, action: .brightNess,value: brightness, zone: zone)
-            
-        case CharacteristicType.saturation:
-            
-            let saturation = characteristic.value as! Float
-            if (saturation > 0){
-                driver.executeCommand(mode: .rgbwwcw, action: .hue, value: saturation, zone: zone)
-            }else{
-                Debugger.shared.log(debugLevel:.Native(logType:.info), "Switching to dedicated whitemode a.k.a cold white)")
-                driver.executeCommand(mode: .rgbwwcw, action: .temperature, value:100,  zone: zone)
-            }
-            
-        default:
-            Debugger.shared.log(debugLevel: .Warning, "Unhandled characteristic change for accessory \(name)")
-        }
-        
-    }
-    
+	
+	let name:String
+	let driver:MilightDriver
+	let zone:MilightZone
+	
+	public init(name:String,
+				driver:MilightDriver,
+				zone:MilightZone
+	) {
+		self.name = name
+		self.driver = driver
+		self.zone = zone
+	}
+	
+	func handleCharacteristicChange<T>(accessory:Accessory,
+									   service: Service,
+									   characteristic: GenericCharacteristic<T>,
+									   to value: T?){
+		
+		// Handle Characteristic change depending on its type
+		switch characteristic.type{
+			case CharacteristicType.powerState:
+				
+				let poweredOn = characteristic.value as! Bool
+				let action = poweredOn ? MilightAction.on : MilightAction.off
+				driver.executeCommand(mode: .rgbwwcw, action: action, zone: zone)
+				
+			case CharacteristicType.brightness:
+				
+				let brightness = characteristic.value as! Int
+				driver.executeCommand(mode: .rgbwwcw, action: .brightNess,value: brightness, zone: zone)
+				
+			case CharacteristicType.hue:
+				
+				let hue = characteristic.value as! Float
+				driver.executeCommand(mode: .rgbwwcw, action: .hue, value: hue, zone: zone)
+
+			case CharacteristicType.saturation:
+				
+				let saturation = characteristic.value as! Float
+				if (saturation > 0){
+					driver.executeCommand(mode: .rgbwwcw, action: .hue, value: saturation, zone: zone)
+				}else{
+					Debugger.shared.log(debugLevel:.Native(logType:.info), "Switching to dedicated whitemode a.k.a cold white)")
+					driver.executeCommand(mode: .rgbwwcw, action: .temperature, value:100,  zone: zone)
+				}
+				
+			default:
+				Debugger.shared.log(debugLevel: .Warning, "Unhandled characteristic change for accessory \(name)")
+		}
+		
+	}
+	
 }
 
 
