cb30f8c5f6f5b1244b5bb87c2dda84c603a9a70a
TheMisfit68 <janverrept@me.com>
2021-02-01 15:55:14 +0100

    Restored Milight controllers

--- HAPiNestApp.swift
+++ HAPiNestApp.swift
@@ -49,7 +49,7 @@ struct HAPiNestApp: App {
 		// Only fire Up PLC after all components are initialized
 		AppState.shared.plc.run()
 		
-		//		AppState.shared.homekitServer.mainBridge.removeAccessoryWith(SerialNumbers: ["00501"])
+//				AppState.shared.homekitServer.mainBridge.removeAccessoryWith(SerialNumbers: ["00001"])
 	}
 	
 	
