e640b7fd7c938d9c78d15196bd056e9ee2efe20d
Jan Verrept <janverrept@me.com>
2021-08-05 23:46:16 +0200

    Testing for the HAP-config file (in the right place)

---
+++ HAPiNestTests/HAPTests.swift
@@ -0,0 +1,34 @@
+//
+//  HAPTests.swift
+//  HAPTests
+//
+//  Created by Jan Verrept on 04/08/2021.
+//  Copyright © 2021 Jan Verrept. All rights reserved.
+//
+import Foundation
+import HAP
+import XCTest
+import JVCocoa
+
+class HAPtests: XCTestCase {
+	
+	func testHAPSupportFolder() throws {
+		
+		let hapSupportFolder = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("/Library/Application Support/HAP")
+		var isFolder:ObjCBool = false
+		let supportFolderExists = FileManager.default.fileExists(atPath: hapSupportFolder.path, isDirectory: &isFolder) && isFolder.boolValue
+		XCTAssertTrue(supportFolderExists)
+		
+	}
+	
+	func testHAPconfigFile() throws {
+		
+		let hapSupportFolder = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("/Library/Application Support/HAP")
+		let configFile = hapSupportFolder.appendingPathComponent("configuration.json")
+		let hapStorage = FileStorage(filename: configFile.path)
+		let configData = try hapStorage.read()
+		XCTAssertNotEqual(configData.count, 0)
+		
+	}
+	
+}
