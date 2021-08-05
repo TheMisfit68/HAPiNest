3a294828e2a186518f19653c0834f25135f128a0
Jan Verrept <janverrept@me.com>
2021-08-05 23:47:21 +0200

    Using the Application support file for the HAP-configdata

--- HAPiNest/HomekitServer/Bridge.swift
+++ HAPiNest/HomekitServer/Bridge.swift
@@ -14,56 +14,71 @@ import JVCocoa
 typealias Bridge = Device
 
 extension Bridge{
-    
-    static let serialNumber = "00001"
+	
+	static let serialNumber = "00001"
 	static var configFile:Storage!
-
+	
 	convenience init(name:String, setupCode:String, accessories:[HAP.Accessory], configfileName:String) {
 		
-		Self.configFile = FileStorage(filename: configfileName)
+		// Use the Application support folder to hold the HAP-configuration-data
+		let fileManager = FileManager.default
+		if let hapSupportDir = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first?.appendingPathComponent("HAP"){
+			
+			fileManager.checkForDirectory(hapSupportDir,createIfNeeded: true)
+			let configFile = hapSupportDir.appendingPathComponent(configfileName)
+			Self.configFile = FileStorage(filename:configFile.path)
+			
+			// Create an Alias for easy access
+			let homeDir = FileManager.default.homeDirectoryForCurrentUser
+			let aliasURL = homeDir.appendingPathComponent("HAP-Data")
+			if !fileManager.fileExists(atPath: aliasURL.path){
+				fileManager.createAlias(from: hapSupportDir, at: aliasURL)
+			}
+			
+		}
 		
-        self.init(
+		self.init(
 			bridgeInfo: Service.Info(name: name, serialNumber: Self.serialNumber),
-            setupCode: Device.SetupCode(stringLiteral: setupCode),
+			setupCode: Device.SetupCode(stringLiteral: setupCode),
 			storage: Self.configFile,
-            accessories: accessories)
-        printPairingInstructions()
-        delegate = self
-    }
-    
-    // MARK: - Pairing Info
-    
-    public func printPairingInstructions(){
-        if isPaired {
-            print()
-            Debugger.shared.log(debugLevel: .Succes, "The bridge is paired, unpair using your iPhone.")
-            print()
-        } else {
-            print()
-            Debugger.shared.log(debugLevel: .Native(logType:.info), "Scan the following QR code using your iPhone to pair this bridge:")
-            print()
-            print(setupQRCode.asText)
-            print(setupCode)
-            print()
-        }
-    }
-    
-    public func resetPairingInfo(){
-        Debugger.shared.log(debugLevel: .Native(logType:.info), "Dropping all pairings, keys")
-        try? Self.configFile.write(Data())
-    }
+			accessories: accessories)
+		printPairingInstructions()
+		delegate = self
+	}
+	
+	// MARK: - Pairing Info
+	
+	public func printPairingInstructions(){
+		if isPaired {
+			print()
+			Debugger.shared.log(debugLevel: .Succes, "The bridge is paired, unpair using your iPhone.")
+			print()
+		} else {
+			print()
+			Debugger.shared.log(debugLevel: .Native(logType:.info), "Scan the following QR code using your iPhone to pair this bridge:")
+			print()
+			print(setupQRCode.asText)
+			print(setupCode)
+			print()
+		}
+	}
+	
+	public func resetPairingInfo(){
+		Debugger.shared.log(debugLevel: .Native(logType:.info), "Dropping all pairings, keys")
+		try? Self.configFile.write(Data())
+	}
 	
 	subscript(accessoryName: String) -> Accessory?{
 		return accessories.first(where: {$0.info.name.value == accessoryName })
 	}
 	
-    public func removeAccessoryWith(SerialNumbers:[String]){
-        
-        let unwantedAccessories = accessories.filter{SerialNumbers.contains($0.info.serialNumber.value!)}
-        removeAccessories(unwantedAccessories, andForgetSerialNumbers: true)
-        
-    }
-    
+	public func removeAccessoryWith(SerialNumbers:[String]){
+		
+		let unwantedAccessories = accessories.filter{SerialNumbers.contains($0.info.serialNumber.value!)}
+		removeAccessories(unwantedAccessories, andForgetSerialNumbers: true)
+		
+	}
+	
 }
 
 
@@ -72,23 +87,23 @@ extension Bridge{
 typealias BridgeDelegate = DeviceDelegate
 
 extension Bridge:BridgeDelegate{
-    
-    func accessoryDelegateFor(accesoryNamed accessoryName:String)->AccessoryDelegate?{
-        return MainConfiguration.HomeKit.Accessories.first(where: {$0.0.info.name.value! == accessoryName} )?.1
-    }
-    
-    public func characteristic<T>(
-        _ characteristic: GenericCharacteristic<T>,
-        ofService: Service,
-        ofAccessory: Accessory,
-        didChangeValue: T?){
-        
-        let accessoryName = ofAccessory.info.name.value!
-        Debugger.shared.log(debugLevel: .Event, "Value '\(characteristic.description ?? "")' of '\(accessoryName)' changed to \(didChangeValue ?? "" as! T)")
-        
-        // Forward all characteristic changes to the corresponding delegate
+	
+	func accessoryDelegateFor(accesoryNamed accessoryName:String)->AccessoryDelegate?{
+		return MainConfiguration.HomeKit.Accessories.first(where: {$0.0.info.name.value! == accessoryName} )?.1
+	}
+	
+	public func characteristic<T>(
+		_ characteristic: GenericCharacteristic<T>,
+		ofService: Service,
+		ofAccessory: Accessory,
+		didChangeValue: T?){
+		
+		let accessoryName = ofAccessory.info.name.value!
+		Debugger.shared.log(debugLevel: .Event, "Value '\(characteristic.description ?? "")' of '\(accessoryName)' changed to \(didChangeValue ?? "" as! T)")
+		
+		// Forward all characteristic changes to the corresponding delegate
 		let accessoryDelegate = accessoryDelegateFor(accesoryNamed:accessoryName)
 		accessoryDelegate?.handleCharacteristicChange(accessory:ofAccessory, service: ofService, characteristic: characteristic, to: didChangeValue)
-    }
-    
+	}
+	
 }
