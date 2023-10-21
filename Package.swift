b4196d527b44ffcb55108baf2896ad86f48ce037
TheMisfit68 <janverrept@me.com>
2023-10-21 23:55:55 +0200

    changed some project-settings

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "HAP",
+    platforms: [
+        .macOS(.v11),
+    ],
+    products: [
+        .library(name: "HAP", targets: ["HAP"]),
+        .executable(name: "hap-demo", targets: ["HAPDemo"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Bouke/SRP.git", from: "3.2.0"),
+        .package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.13.0"),
+        .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.0"),
+    ],
+    targets: [
+        .target(name: "CQRCode", exclude: ["LICENSE.txt"]),
+        .target(name: "COperatingSystem", exclude: ["LICENSE"]),
+        .target(name: "VaporHTTP",
+                dependencies: [
+                    .product(name: "NIO", package: "swift-nio"),
+                    .product(name: "NIOHTTP1", package: "swift-nio"),
+                    .product(name: "NIOFoundationCompat", package: "swift-nio"),
+                    "COperatingSystem",
+                ],
+                exclude: ["LICENSE"]),
+        .target(name: "HAP",
+                dependencies: [
+                    "SRP",
+                    .product(name: "Logging", package: "swift-log"),
+                    "Regex",
+                    "CQRCode",
+                    "VaporHTTP",
+                    .product(name: "Crypto", package: "swift-crypto"),
+                ],
+                exclude: ["Base/Predefined/README"]),
+        .executableTarget(name: "HAPDemo",
+                          dependencies: [
+                            "HAP",
+                            .product(name: "Logging", package: "swift-log")
+                          ]),
+        .testTarget(name: "HAPTests", dependencies: ["HAP"]),
+    ]
+)
+
+#if os(macOS)
+    package.products.append(.executable(name: "hap-update", targets: ["HAPInspector"]))
+    package.targets.append(.executableTarget(name: "HAPInspector"))
+#endif
+
+#if os(Linux)
+    package.dependencies.append(.package(url: "https://github.com/Bouke/NetService.git", from: "0.7.0"))
+    package.targets.first(where: { $0.name == "HAP" })!.dependencies.append("NetService")
+#endif
--- ToDo.md
+++ ToDo.md
@@ -1,8 +1,17 @@
 # ToDo for HAPiNest
 ## A personal Home Automation System
 
-- [ ] Replace openwheater with weatherkit from Apple
-- [ ] Create a View/Container and Preference view/container for each module
+
+- [ ]  Implement electric car accessory
+_(Complete ACController with temperature setpoint if Nissan developer account gets granted)_
+
+- [ ]  Write DOCC files 
+For Siridriver
+For ApplescriptDriver
+For Hapinest with UML diagrams to explain architecture
+
+- [ ] Create a View/Container and Preference view/container for each module,
+use the. WeatherService.CreditsView as an example
 - [ ] Create Preference Window that show those views in a their own tab
 
 - [ ] Add driver for IP Cams
@@ -12,7 +21,7 @@
 - [ ] Bundle ClibYASDI as a binary package by creating a new binary-target for the package (do not includec zip-archive in git, upload to seperate server LFS)
 
 ## Longterm MacOS xx
-Use Swift-Regex for all future regex parsing
-- [ ] Refactor TizenDriver (=Websocket) to Async/Await when available
-- [ ] Refactor Milightdriver (=UDP-socket) to Async/Await when available
+- [ ] Refactor TizenDriver (=Websocket) to Async/Await when available.
+- [ ] Refactor Milightdriver (=UDP-socket) to Async/Await when available.
+- [ ] Use SwiftData as the persistent store for all dbase-based Apps.
 
