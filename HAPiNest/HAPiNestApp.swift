888325ee1c5f0998fc6e8b9f67501df6d53188de
TheMisfit68 <janverrept@me.com>
2023-10-29 18:21:57 +0100

    DOCS: added some DOCC to the app

--- HAPiNest/HAPiNestApp.swift
+++ HAPiNest/HAPiNestApp.swift
@@ -15,6 +15,12 @@ import HAP
 import SoftPLC
 import ModbusDriver
 
+/// The main App that sets up a HomeKit-server together with its HomeKit-bridge
+/// and a number of hardwaredrivers that will act as Accessory-delegates.
+/// The hardwaredrivers/Accessory-delegates react to changes made to an accessory when it is controlled in the Home-app,
+/// by translating those changes into actions for the hardware.
+/// The main accessory-delegate in this App is a SoftPLC that can interact with the outside world by means of a number of Input and Output-modules.
+ 
 @main
 struct HAPiNestApp: App {
     @Environment(\.scenePhase) var scenePhase
@@ -27,8 +33,6 @@ struct HAPiNestApp: App {
     
     init() {
         
-        plc.plcObjects = MainConfiguration.PLC.PLCobjects
-        
         let bridgename = MainConfiguration.HomeKitServer.BridgeName
         let setupCode = MainConfiguration.HomeKitServer.BridgeSetupCode
         let configFile = MainConfiguration.HomeKitServer.BridgeConfigFile
@@ -40,13 +44,13 @@ struct HAPiNestApp: App {
             configfileName: configFile
         )
         
+        plc.plcObjects = MainConfiguration.PLC.PLCobjects
 #if DEBUG
         plc.toggleSimulator(true)
         plc.toggleHardwareSimulation(true)
 #else
         plc.toggleSimulator(false)
 #endif
-        
         // Only fire Up PLC after all components are initialized
         plc.run()
         
